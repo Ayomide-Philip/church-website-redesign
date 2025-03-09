828b0370355d71bd437231b21e4c6e064787cd6f
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-09 17:33:37 +0100

    Update routing to include about page and 404 handling

--- src/route.jsx
+++ src/route.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-
+import Page404 from "./pages/404";
+import App from "./App";
+import "./output.css";
 function AppRouting() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Home />} />
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<Page404 />}>
+        <Route path="history" element={<Home />} />
+      </Route>
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 }
