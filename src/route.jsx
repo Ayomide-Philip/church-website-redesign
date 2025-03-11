5081bd0966432444dea8ce1959472812a7f1ee23
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-11 09:37:00 +0100

    Replace App component with Header in routing for improved navigation

--- src/route.jsx
+++ src/route.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Page404 from "./pages/404";
-import App from "./App";
+import Header from "./componet/header";
 import "./output.css";
 function AppRouting() {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Header />} />
       <Route path="/about" element={<Page404 />}>
         <Route path="history" element={<Home />} />
       </Route>
