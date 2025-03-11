f64e1f1df173ab10f559e33b4a00531ff31f2bf9
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-11 21:18:59 +0100

    Add Header component to Home page for improved layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import Header from "../components/Header";
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>This is the Home Page</h1>
     </div>
   );
