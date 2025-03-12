7ed57fb2ec619d074e23ee9a1b651e6fbfe40910
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-12 20:38:55 +0100

    Update routing to display Home component on root path and fix Header import

--- src/route.jsx
+++ src/route.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Page404 from "./pages/404";
-import Header from "./componet/header";
 import "./output.css";
 
 function AppRouting() {
   return (
     <Routes>
-      <Route path="/" element={<Header />} />
-      <Route path="/about" element={<Page404 />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/about">
         <Route path="history" element={<Home />} />
       </Route>
       <Route path="*" element={<Page404 />} />
