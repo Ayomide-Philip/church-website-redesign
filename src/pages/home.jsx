337fb8634a6ad84c318effc6c52c3d1469158030
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-13 18:22:53 +0100

    Update VideoBanner component in Home page to refine title and subtitle text

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <VideoBanner title="Welcome To Gateway Baptist Church" subtitle="...Community of Grace" />
+      <VideoBanner title="Welcome To Gateway Baptist Church." subtitle="... Community of Grace." />
     </div>
   );
 }
