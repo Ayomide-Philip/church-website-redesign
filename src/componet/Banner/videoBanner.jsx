ec704f2d081b4331376fb3553cbb1a87d6e29efe
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-13 16:41:41 +0100

    Fix video source path and update heading class for improved styling

--- src/componet/Banner/videoBanner.jsx
+++ src/componet/Banner/videoBanner.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function VideoBanner() {
   return (
     <div class="video-container">
-      <video autoplay muted loop>
-        <source src="/video/3373659-hd_1920_1080_24fps.mp4" type="video/mp4" />
+      <video autoPlay muted loop>
+        <source src="/videos/3373659-hd_1920_1080_24fps.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div class="video-overlay">
         <div>
-          <h1 class="display-4">Welcome to Our Church</h1>
+          <h1 class="text-6xl">Welcome to Our Church</h1>
           <p class="lead">Join us for worship and community</p>
         </div>
       </div>
