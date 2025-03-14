8d7f7bf91b3b64426ea06c7e41b458382392b7c6
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-14 21:59:15 +0100

    Comment out unused span elements in Team component

--- src/componet/Home/team.jsx
+++ src/componet/Home/team.jsx
@@ -149,7 +149,7 @@ export default function Team() {
                   DIY tote bag drinking vinegar cronut adaptogen squid fanny
                   pack vaporware.
                 </p>
-                <span class="inline-flex">
+                {/* <span class="inline-flex">
                   <a class="text-gray-500">
                     <svg
                       fill="none"
@@ -189,7 +189,7 @@ export default function Team() {
                       <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                     </svg>
                   </a>
-                </span>
+                </span> */}
               </div>
             </div>
           </div>
@@ -209,7 +209,7 @@ export default function Team() {
                   DIY tote bag drinking vinegar cronut adaptogen squid fanny
                   pack vaporware.
                 </p>
-                <span class="inline-flex">
+                {/* <span class="inline-flex">
                   <a class="text-gray-500">
                     <svg
                       fill="none"
@@ -249,7 +249,7 @@ export default function Team() {
                       <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                     </svg>
                   </a>
-                </span>
+                </span> */}
               </div>
             </div>
           </div>
