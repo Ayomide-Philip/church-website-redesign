6d2a349beaf17767986766e05c28e825a7011494
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-14 12:43:54 +0100

    Update 404 page layout and styling, add Header component

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,21 @@
+import Header from "../componet/Header/header";
 export default function Page404() {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <Header />
+      <main className="grid min-h-full place-items-center bg-zinc-600 px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+          
+          <p className="text-3xl font-semibold text-sky-500/100">404</p>
+
+          <h1 className="mt-3 text-5xl font-semibold tracking-tight text-balance text-white  sm:text-7xl">
             Page not found
           </h1>
+
           <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
+
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="/"
@@ -18,6 +24,7 @@ export default function Page404() {
               Go back home
             </a>
           </div>
+
         </div>
       </main>
     </>
