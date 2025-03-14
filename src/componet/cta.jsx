943aad71ea24e3dc63f44182f53358f531ea08e7
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-14 21:26:04 +0100

    Update CTA component image source for improved asset management

--- src/componet/cta.jsx
+++ src/componet/cta.jsx
@@ -61,7 +61,7 @@ export default function Cta() {
           </div>
           <img
             alt="Product screenshot"
-            src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
+            src="/images/cta-image.jpg"
             width={2432}
             height={1442}
             className="w-[48rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
