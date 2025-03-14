85de1d7b44585f53948e5c9db7a2e27629ba1e69
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-14 21:08:11 +0100

    Add CTA component to home page for enhanced user engagement

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "../componet/Header/header";
 import VideoBanner from "../componet/Banner/videoBanner";
+import Cta from "../componet/cta";
 export default function Home() {
   return (
     <div>
       <Header />
-      <VideoBanner title="Welcome To Gateway Baptist Church." subtitle="... Community of Grace." />
+      <VideoBanner
+        title="Welcome To Gateway Baptist Church."
+        subtitle="... Community of Grace."
+      />
+      <Cta />
     </div>
   );
 }
