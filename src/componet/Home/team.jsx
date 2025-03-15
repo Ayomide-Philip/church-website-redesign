f0a05196d413d68828b3b87199d52480a9989b42
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-15 13:07:32 +0100

    Add button to Team component for enhanced user interaction

--- src/componet/Home/team.jsx
+++ src/componet/Home/team.jsx
@@ -66,6 +66,9 @@ export default function Team() {
             );
           })}
         </div>
+        <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Button
+        </button>
       </div>
     </section>
   );
