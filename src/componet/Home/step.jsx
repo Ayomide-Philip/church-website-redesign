19ecbeb123087db65f5beb00449610b0dbc1904f
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-15 14:22:31 +0100

    Implement dynamic Step component with mapped steps for enhanced guidance

--- src/componet/Home/step.jsx
+++ src/componet/Home/step.jsx
@@ -1,10 +1,61 @@
+const step = [
+  {
+    title: "STEP 1",
+    description:
+      "VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.",
+  },
+  {
+    title: "STEP 2",
+    description:
+      "Vice migas literally kitsch +1 pok pok. Truffaut hot chicken slow-carb health goth, vape typewriter.",
+  },
+  {
+    title: "STEP 3",
+    description:
+      "Coloring book nar whal glossier master cleanse umami. Salvia +1 master cleanse blog taiyaki.",
+  },
+  {
+    title: "STEP 4",
+    description:
+      "VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.",
+  },
+];
 export default function Step() {
   return (
     <section class="text-gray-400 bg-gray-900 body-font">
       <div class="container px-5 py-24 mx-auto flex flex-wrap">
         <div class="flex flex-wrap w-full">
           <div class="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
-            <div class="flex relative pb-12">
+            {step.map((steps) => {
+              return (
+                <div class="flex relative pb-12">
+                  <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+                    <div class="h-full w-1 bg-gray-800 pointer-events-none"></div>
+                  </div>
+                  <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                  <div class="flex-grow pl-4">
+                    <h2 class="font-medium title-font text-sm text-white mb-1 tracking-wider">
+                      {steps.title}
+                    </h2>
+                    <p class="leading-relaxed">{steps.description}</p>
+                  </div>
+                </div>
+              );
+            })}
+            {/* <div class="flex relative pb-12">
               <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
                 <div class="h-full w-1 bg-gray-800 pointer-events-none"></div>
               </div>
@@ -114,6 +165,7 @@ export default function Step() {
                 </p>
               </div>
             </div>
+
             <div class="flex relative">
               <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
                 <svg
@@ -138,7 +190,7 @@ export default function Step() {
                   cold-pressed retro.
                 </p>
               </div>
-            </div>
+            </div> */}
           </div>
           <img
             class="lg:w-3/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12"
