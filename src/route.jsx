a15a7e84878142e93bf3a1dc2628afdbea1d29a4
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-15 21:19:04 +0100

    Update routing to include History page and remove nested About route

--- src/route.jsx
+++ src/route.jsx
@@ -3,14 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Page404 from "./pages/404";
 import "./output.css";
+import History from "./pages/history";
 
 function AppRouting() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about">
-        <Route path="history" element={<Home />} />
-      </Route>
+      <Route path="/history" element={<History />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
   );
