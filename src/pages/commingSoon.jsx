d652e9eb78aa50eee898d470e2b24af48075951b
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-15 21:47:49 +0100

    Add Coming Soon page with header, footer, and background image

---
+++ src/pages/commingSoon.jsx
@@ -0,0 +1,23 @@
+import Header from "../componet/Header/header";
+import Footer from "../componet/Footer/footer";
+
+export default function CommingSoon() {
+  return (
+    <>
+      <Header />
+      <div class="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4">
+        <div
+          class="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-30"
+          style={{
+            backgroundImage:
+              "url('https://images.unsplash.com/photo-1604093882750-3ed498f3178b');",
+          }}
+        ></div>
+        <h1 class="text-5xl md:text-7xl text-white font-bold mb-8 z-10">
+          Coming Soon
+        </h1>
+      </div>
+      <Footer />
+    </>
+  );
+}
