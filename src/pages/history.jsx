e8d653b99dd44fe0ee2ce77f1c89252fd8114ff7
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-15 21:29:49 +0100

    Add History page with Header and Footer components

---
+++ src/pages/history.jsx
@@ -0,0 +1,13 @@
+import Header from "../componet/Header/header";
+import Footer from "../componet/Footer/footer";
+import VideoBanner from "../componet/Banner/videoBanner";
+
+export default function History() {
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
+  );
+}
