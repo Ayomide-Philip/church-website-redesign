272f3478c00cd3a802fd2d7f6daeed4a7c894bd4
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-15 13:12:01 +0100

    Update Team component styles and button text for improved presentation

--- src/componet/Home/team.jsx
+++ src/componet/Home/team.jsx
@@ -32,7 +32,7 @@ const team = [
 export default function Team() {
   return (
     <section class="text-gray-400 bg-gray-900 body-font">
-      <div class="container px-5 py-24 mx-auto">
+      <div class="container px-5 py-5 mx-auto">
         <div class="flex flex-col text-center w-full mb-20">
           <h1 class="text-3xl font-medium title-font mb-4 text-white tracking-widest">
             OUR TEAM
@@ -66,8 +66,8 @@ export default function Team() {
             );
           })}
         </div>
-        <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-          Button
+        <button class="flex mx-auto mt-10 text-white bg-black border-0 py-2 px-10 focus:outline-none hover:bg-white hover:text-black rounded text-lg">
+          Meet The Team
         </button>
       </div>
     </section>
