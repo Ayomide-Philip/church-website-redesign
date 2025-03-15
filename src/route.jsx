9122d29be2ab4c04ba7898f9b07eafa76d5957dd
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-15 22:01:31 +0100

    Add route for Coming Soon page

--- src/route.jsx
+++ src/route.jsx
@@ -4,12 +4,14 @@ import Home from "./pages/home";
 import Page404 from "./pages/404";
 import "./output.css";
 import History from "./pages/history";
+import CommingSoon from "./pages/commingSoon";
 
 function AppRouting() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/history" element={<History />} />
+      <Route path="/missionandvission" element={<CommingSoon />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
   );
