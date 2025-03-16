4075187bd95d8f6a23a4e1b8e7f642b5ef03c538
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-16 20:44:49 +0100

    Add WhyNameGateway component for historical context on church naming

---
+++ src/componet/History/whyNameGateway.jsx
@@ -0,0 +1,76 @@
+export default function WhyNameGateway() {
+  return (
+    <div class="relative p-4">
+      <div class="max-w-3xl mx-auto">
+        <div class="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+          <div class="">
+            <h3 class="text-2xl font-bold my-5">Why the name was "Gateway" Baptist Church?</h3>
+            <p class="text-base leading-8 my-5">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <blockquote class="text-md italic leading-8 my-5 p-5 text-indigo-600 font-semibold">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </blockquote>
+            <p class="text-base leading-8 my-5">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <a
+              href="./#"
+              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
+            >
+              #Election
+            </a>
+            ,
+            <a
+              href="./#"
+              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
+            >
+              #people
+            </a>
+            ,
+            <a
+              href="./#"
+              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
+            >
+              #Election2020
+            </a>
+            ,
+            <a
+              href="./#"
+              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
+            >
+              #trump
+            </a>
+            ,
+            <a
+              href="./#"
+              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
+            >
+              #Joe
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
