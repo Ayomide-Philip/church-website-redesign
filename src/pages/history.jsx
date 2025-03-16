aa8ba647f3d59b9bec87791c91b1edb4c4f97e3f
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-16 20:19:46 +0100

    Rename ContentSection import to FoundingandEstablishing in History component

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -1,7 +1,7 @@
 import Header from "../componet/Header/header";
 import Footer from "../componet/Footer/footer";
 import VideoBanner from "../componet/Banner/videoBanner";
-import ContentSection from "../componet/History/contentSection";
+import FoundingandEstablishing from "../componet/History/contentSection";
 export default function History() {
   return (
     <>
@@ -11,7 +11,7 @@ export default function History() {
         title="The Church In the Historical Perspective."
         subtitle="Learn more about our church history."
       />
-      <ContentSection />
+      <FoundingandEstablishing />
       <Footer />
     </>
   );
