94407d8e5c471de832f8de35819626ed2da641c0
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-16 19:58:11 +0100

    Update ContentSection heading and comment out unused paragraph

--- src/componet/History/contentSection.jsx
+++ src/componet/History/contentSection.jsx
@@ -42,11 +42,11 @@ export default function ContentSection() {
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="lg:max-w-lg">
-              <p className="text-base/7 font-semibold text-indigo-600">
+              {/* <p className="text-base/7 font-semibold text-indigo-600">
                 Deploy faster
-              </p>
+              </p> */}
               <h1 className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
-                A better workflow
+                Founding and Establishing the Church.
               </h1>
               <p className="mt-6 text-xl/8 text-gray-700">
                 Aliquet nec orci mattis amet quisque ullamcorper neque, nibh
