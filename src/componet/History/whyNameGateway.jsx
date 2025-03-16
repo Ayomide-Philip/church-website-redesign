44f54791d20f7807689594ccb96d236d6ce9b48b
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-16 20:47:46 +0100

    Update WhyNameGateway component with detailed historical context and remove unused links

--- src/componet/History/whyNameGateway.jsx
+++ src/componet/History/whyNameGateway.jsx
@@ -4,18 +4,22 @@ export default function WhyNameGateway() {
       <div class="max-w-3xl mx-auto">
         <div class="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
           <div class="">
-            <h3 class="text-2xl font-bold my-5">Why the name was "Gateway" Baptist Church?</h3>
+            <h3 class="text-2xl font-bold my-5">
+              Why the name was "Gateway" Baptist Church?
+            </h3>
             <p class="text-base leading-8 my-5">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              Have you ever wondered why the church was named "Gateway"? As at
+              1960, there was only an entry point from the northern part of the
+              country. At the site are of the church was the airport. Also, it
+              was and still is the only south-north route for the Nigeria
+              Railway. At that time, hardly could anybody come to visit the
+              Western Region without passing through the Sanngo area of the
+              city. The worshippers, through the divine inspiration as voiced by
+              Deacon Lawore, then leader of the preaching station, then
+              considered "GATEWAY" BAPTIST CHURCH as the name because it was,
+              and still is the Gateway to Heaven, Paradise, Saviour and
+              salvation as well as, break-through, success, miracles, blessings,
+              redemption and long life and prosperity.
             </p>
             <blockquote class="text-md italic leading-8 my-5 p-5 text-indigo-600 font-semibold">
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -34,40 +38,6 @@ export default function WhyNameGateway() {
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum.
             </p>
-            <a
-              href="./#"
-              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
-            >
-              #Election
-            </a>
-            ,
-            <a
-              href="./#"
-              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
-            >
-              #people
-            </a>
-            ,
-            <a
-              href="./#"
-              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
-            >
-              #Election2020
-            </a>
-            ,
-            <a
-              href="./#"
-              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
-            >
-              #trump
-            </a>
-            ,
-            <a
-              href="./#"
-              class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
-            >
-              #Joe
-            </a>
           </div>
         </div>
       </div>
