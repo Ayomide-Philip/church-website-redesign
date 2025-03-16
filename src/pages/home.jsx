357dd5ed9f91b7fec7aa193d87eeb103748966c4
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-16 17:56:42 +0100

    Refactor Home component to clean up commented-out install button code

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -100,11 +100,7 @@ export default function Home() {
 
   return (
     <div>
-      {/* {isInstallButtonVisible && (
-        <button id="install-button" onClick={handleInstallClick}>
-          Install
-        </button>
-      )} */}
+     
       {isInstallButtonVisible && (
         <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
           <div
