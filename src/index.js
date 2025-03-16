09ced1520c0681f74c679243ef9063b01a179d71
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-16 16:39:46 +0100

    Update homepage URL format in package.json and switch from HashRouter to BrowserRouter in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./output.css";
 import AppRouting from "./route";
 
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppRouting />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
