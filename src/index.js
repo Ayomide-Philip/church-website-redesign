1fe553e9859fff451c4d8fb75bbdcca52f7bbede
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-16 16:24:32 +0100

    Replace BrowserRouter with HashRouter for routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./output.css";
 import AppRouting from "./route";
 
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppRouting />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
