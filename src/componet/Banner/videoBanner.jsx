2383998730e2a869c50a224f49490a90a54b3936
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-17 14:49:13 +0100

    Add typing effect to title in VideoBanner component

--- src/componet/Banner/videoBanner.jsx
+++ src/componet/Banner/videoBanner.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { Typed } from "react-typed";
 
 function VideoBanner(props) {
   return (
     <div class="video-container">
       <video autoPlay muted loop>
-     
         <source src={`/videos/${props.video}.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div class="video-overlay">
         <div>
-          <h1 class="text-4xl md:text-5xl lg:text-6xl pb-2"> {props.title}</h1>
+          <h1 class="text-4xl md:text-5xl lg:text-6xl pb-2 typing-text">
+            {props.title}
+          </h1>
           <p class="text-1xl md:text-2xl lg:text-3xl">{props.subtitle}</p>
         </div>
       </div>
