a9c25717d3fb89dc940c7f118e90776490964e1a
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-17 13:57:18 +0100

    Refactor WhyNameGateway component for improved styling and layout consistency

--- src/componet/History/whyNameGateway.jsx
+++ src/componet/History/whyNameGateway.jsx
@@ -1,7 +1,7 @@
 export default function WhyNameGateway() {
   return (
     <div className="relative p-4">
-      <div className="max-w-3xl bg-[#F8F9FA] mx-auto">
+      <div className="max-w-3xl  mx-auto">
         <div className="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
           <div className="">
             <h3 className="text-2xl font-bold my-5">
