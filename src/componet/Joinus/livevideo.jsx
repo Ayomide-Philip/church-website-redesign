c56456fb40e538c61b176c00751d9e45495a98ee
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-18 22:13:34 +0100

    Update LiveVideo component title for improved clarity and engagement

--- src/componet/Joinus/livevideo.jsx
+++ src/componet/Joinus/livevideo.jsx
@@ -1,7 +1,11 @@
+
+
 export default function LiveVideo() {
   return (
     <main class="flex-grow flex flex-col space-y-5 items-center bg-stone-300 justify-center p-4">
-      <h1>Watch us life Today</h1>
+      <h1 class="sm:text-4xl py-5 capitalize  font-bold title-font mb-4 text-black tracking-widest">
+        Experience The Power Of Worship - Watch us Live Today
+      </h1>
       <div class="w-full md:w-2/3 max-w-3xl mx-auto">
         <div class="relative pb-[56.25%] rounded-lg overflow-hidden">
           <iframe
