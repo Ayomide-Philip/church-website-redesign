d1c0fabb0478b34151dd73435ac139a37efa0415
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-18 20:00:24 +0100

    Add Join Us page with Header, Footer, and video player

---
+++ src/pages/joinus.jsx
@@ -0,0 +1,18 @@
+import Footer from "../componet/Footer/footer";
+import Header from "../componet/Header/header";
+
+export default function JoinUs() {
+  return (
+    <>
+      <Header title="Join Us" />
+      <main class="flex-grow flex flex-col space-y-5 items-center bg-stone-300 justify-center p-4">
+        <video
+          id="videoPlayer"
+          class="w-full md:w-2/3 max-w-3xl mx-auto rounded-lg "
+          controls
+        ></video>
+      </main>
+      <Footer />
+    </>
+  );
+}
