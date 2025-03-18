f2349130b66da150949d8d383b74043727f09d76
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-18 21:54:18 +0100

    Add VideoBanner component to Join Us page for enhanced video display

--- src/pages/joinus.jsx
+++ src/pages/joinus.jsx
@@ -1,3 +1,4 @@
+import VideoBanner from "../componet/Banner/videoBanner";
 import Footer from "../componet/Footer/footer";
 import Header from "../componet/Header/header";
 import LiveVideo from "../componet/Joinus/livevideo";
@@ -5,8 +6,11 @@ import LiveVideo from "../componet/Joinus/livevideo";
 export default function JoinUs() {
   return (
     <>
-      <Header title="Join Us" />
-
+      <Header />
+      <VideoBanner
+        video="3373659-hd_1920_1080_24fps"
+        title="Join Us Today for Church Service"
+      />
       <LiveVideo />
 
       <Footer />
