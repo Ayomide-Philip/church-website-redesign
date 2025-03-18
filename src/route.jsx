a1d3c9a6b26723856409517f7a1d8dacac6f8c40
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-18 20:04:45 +0100

    Update routing to display Join Us page instead of Coming Soon

--- src/route.jsx
+++ src/route.jsx
@@ -5,6 +5,7 @@ import Page404 from "./pages/404";
 import "./output.css";
 import History from "./pages/history";
 import CommingSoon from "./pages/commingSoon";
+import JoinUs from "./pages/joinus";
 
 function AppRouting() {
   return (
@@ -20,7 +21,7 @@ function AppRouting() {
       <Route path="/donate" element={<CommingSoon />} />
       <Route path="/bibleStudyMaterial" element={<CommingSoon />} />
       <Route path="/leaders" element={<CommingSoon />} />
-      <Route path="/joinus" element={<CommingSoon />} />
+      <Route path="/joinus" element={<JoinUs />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
   );
