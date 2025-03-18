2a3b4b7197bfb672fc29e5f4b34278fd83d876f0
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-18 21:35:28 +0100

    Add LiveVideo component for embedding Facebook Live video

---
+++ src/componet/Joinus/livevideo.jsx
@@ -0,0 +1,18 @@
+export default function LiveVideo() {
+  return (
+    <main class="flex-grow flex flex-col space-y-5 items-center bg-stone-300 justify-center p-4">
+      <h1>Watch us life Today</h1>
+      <div class="w-full md:w-2/3 max-w-3xl mx-auto">
+        <div class="relative pb-[56.25%] rounded-lg overflow-hidden">
+          <iframe
+            class="absolute top-0 left-0 w-full h-full"
+            src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F1069831131850092"
+            frameborder="0"
+            allowfullscreen
+            title="Facebook Live Video"
+          ></iframe>
+        </div>
+      </div>
+    </main>
+  );
+}
