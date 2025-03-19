5ff8114ad587dc9003464b8a57fa73ae75e3604c
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-19 12:18:04 +0100

    Remove commented-out Facebook video embed code from JoinUs component

--- src/pages/joinus.jsx
+++ src/pages/joinus.jsx
@@ -17,4 +17,3 @@ export default function JoinUs() {
     </>
   );
 }
-// src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F1069831131850092%2F&show_text=false&width=560&t=0"
