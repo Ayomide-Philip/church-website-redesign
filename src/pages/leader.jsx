b0becaedff7a9dc91c20485061a5e8b5b59682c5
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-19 13:17:13 +0100

    Add Leader component to render header, footer, and OurLeader

---
+++ src/pages/leader.jsx
@@ -0,0 +1,12 @@
+import Header from "../componet/Header/header";
+import Footer from "../componet/Footer/footer";
+import OurLeader from "../componet/Leader/ourLeader";
+export default function Leader() {
+  return (
+    <>
+      <Header />
+      <OurLeader />
+      <Footer />
+    </>
+  );
+}
