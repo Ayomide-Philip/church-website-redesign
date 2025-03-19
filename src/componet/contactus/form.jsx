b7b4c42c6740ee42b715358182e32bdcf1ec5c08
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-19 12:59:58 +0100

    Add required attribute to message textarea in Contact Us form

--- src/componet/contactus/form.jsx
+++ src/componet/contactus/form.jsx
@@ -62,6 +62,7 @@ export default function Form() {
                     id="message"
                     name="message"
                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    required
                   ></textarea>
                 </div>
               </div>
