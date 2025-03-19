173fe25930f9e2bc7a9a9a29a11b7e7e524caec8
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-19 12:52:47 +0100

    Update Contact Us form to replace message textarea with phone number input field

--- src/componet/contactus/form.jsx
+++ src/componet/contactus/form.jsx
@@ -43,13 +43,14 @@ export default function Form() {
               <div class="p-2 w-full">
                 <div class="relative">
                   <label for="message" class="leading-7 text-sm text-gray-600">
-                    Message
+                    Phone Number
                   </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                  ></textarea>
+                  <input
+                    type="number"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none ease-in-out "
+                  />
                 </div>
               </div>
               <div class="p-2 w-full">
@@ -78,3 +79,5 @@ export default function Form() {
     </section>
   );
 }
+
+//rounded-l-none !border-t-blue-gray-200 placeholder:text-blue-gray-300 placeholder:opacity-100  focus:!border-t-gray-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none
