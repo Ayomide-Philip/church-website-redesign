e81e7596abe1d082b26532d69b85b522cf07e1cc
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-26 21:57:06 +0100

    Add new leader entries to OurLeader component

--- src/componet/Leader/ourLeader.jsx
+++ src/componet/Leader/ourLeader.jsx
@@ -87,6 +87,22 @@ const leader = [
     description:
       "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
   },
+  {
+    name: "lorem ipesum",
+    role: "UI Developer",
+    imageName: "deaconate-chairperson",
+    imageLocation: "supportingMinistry",
+    description:
+      "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+  },
+  {
+    name: "lorem ipesum",
+    role: "UI Developer",
+    imageName: "deaconate-chairperson",
+    imageLocation: "supportingMinistry",
+    description:
+      "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+  },
 ];
 export default function OurLeader() {
   return (
