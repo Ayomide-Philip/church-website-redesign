baf3dc5ef58716587c2f728135607d06f2679425
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-27 18:11:15 +0100

    ``` No code changes made. ```

--- src/componet/MissionandVission/missionSection.jsx
+++ src/componet/MissionandVission/missionSection.jsx
@@ -1,30 +1,4 @@
-export default function MissionSection() {
-  return (
-    <div class="w-full h-full bg-white dark:bg-gray-800">
-      <div class="w-full mx-auto py-5 bg-white dark:bg-gray-800">
-        <div class="w-[90%] mx-auto flex md:gap-4 xs:gap-2 justify-center items-center pt-2">
-          <div class="flex gap-2 items-center">
-            <img
-              src="/images/images.png"
-              alt="Church Profile"
-              class="md:w-[2.2rem] md:h-[2.2rem] xs:w-[2rem] xs:h-[2rem] rounded-full"
-            />
-            <h2 class="text-sm font-semibold dark:text-white">
-              Gateway Baptist Church
-            </h2>
-          </div>
-
-          <div class="dark:text-gray-500">|</div>
-          <h4 class="text-sm font-semibold text-gray-600 dark:text-gray-400">
-            5 MIN READ
-          </h4>
-        </div>
-        <h1 class="w-[92%] mx-auto lg:text-4xl md:text-3xl xs:text-2xl text-center font-serif font-semibold pb-2 pt-2 dark:text-white">
-          Our Purpose Statement | Our Mission and Vision as a Church.
-        </h1>
-        <div class="py-2 bg-white dark:bg-gray-800">
-          <div class="md:w-[80%] xs:w-[90%] mx-auto pt-4">
-            <h1 class="font-semibold text-lg mt-4 dark:text-white">
+/* <h1 class="font-semibold text-lg mt-4 dark:text-white">
               1. The Gateway Baptist Church (Community of Grace) exists to
               fulfill the Great Commandment and the great Commission.
             </h1>
@@ -65,39 +39,59 @@ export default function MissionSection() {
               spiritually and numerically. These group activities provide
               opportunities for individuals to connect with one another, share
               their faith, and support each other in their spiritual journeys.
-            </p>
+            </p> */
 
-            <h1 class="font-semibold text-lg mt-4 dark:text-white">
-              4. Customization King
-            </h1>
-            <p class="mt-2 text-md dark:text-gray-300">
-              Don't be fooled by Tailwind's utility-first approach. You can
-              still create custom themes and components. Need a specific button
-              style? No problem, define it with custom CSS and reuse it
-              throughout your project. Tailwind integrates seamlessly with your
-              existing workflow.
-            </p>
+const purposeStatement = [
+  {
+    id: 1,
+    heading:
+      "The Gateway Baptist Church (Community of Grace) exists to fulfill the Great Commandment and the great Commission.",
+    paragraph:
+      "In Matthew 22:37-40, Jesus emphasizes the importance of loving God with all our heart, soul, and mind, and loving our neighbors as  ourselves, stating that all the law and the Prophets are based on  these two commandments. In Matthew 28:19-20, Jesus commands his followers to go and make disciples of all nations, baptizing them  in the name of the Father, Son, and Holy Spirit, and teaching them  to obey all that He has commanded. Both passages highlight the  core of Christian teaching: love for God and others, and the mission to spread this love through discipleship and obedience.",
+  },
+];
 
-            <h1 class="font-semibold text-lg mt-4 dark:text-white">
-              5. Framework Agnostic
-            </h1>
-            <p class="mt-2 text-md dark:text-gray-300">
-              Tailwind plays well with others. Whether you're using React, Vue,
-              Angular, or plain JavaScript, Tailwind integrates without a hitch.
-              This flexibility makes it a valuable asset for any project
-              regardless of your preferred framework.
-            </p>
+export default function MissionSection() {
+  return (
+    <div class="w-full h-full bg-white dark:bg-gray-800">
+      <div class="w-full mx-auto py-5 bg-white dark:bg-gray-800">
+        <div class="w-[90%] mx-auto flex md:gap-4 xs:gap-2 justify-center items-center pt-2">
+          <div class="flex gap-2 items-center">
+            <img
+              src="/images/images.png"
+              alt="Church Profile"
+              class="md:w-[2.2rem] md:h-[2.2rem] xs:w-[2rem] xs:h-[2rem] rounded-full"
+            />
+            <h2 class="text-sm font-semibold dark:text-white">
+              Gateway Baptist Church
+            </h2>
+          </div>
 
-            <h1 class="font-semibold text-lg mt-4 dark:text-white">
-              Conclusion
-            </h1>
-            <p class="mt-2 text-md dark:text-gray-300">
-              Tailwind CSS offers a unique approach to styling that prioritizes
-              speed, customization, and responsiveness. It's a powerful tool
-              that can streamline your workflow and help you build beautiful,
-              modern websites. So, if you're looking for a CSS framework that
-              empowers you to create with freedom, give Tailwind CSS a try!
-            </p>
+          <div class="dark:text-gray-500">|</div>
+          <h4 class="text-sm font-semibold text-gray-600 dark:text-gray-400">
+            5 MIN READ
+          </h4>
+        </div>
+        <h1 class="w-[92%] mx-auto lg:text-4xl md:text-3xl xs:text-2xl text-center font-serif font-semibold pb-2 pt-2 dark:text-white">
+          Our Purpose Statement | Our Mission and Vision as a Church.
+        </h1>
+        <div class="py-2 bg-white dark:bg-gray-800">
+          <div class="md:w-[80%] xs:w-[90%] mx-auto pt-4">
+            {purposeStatement.map((purpose) => {
+              return (
+                <>
+                  <h1
+                    key={purpose.id}
+                    class="font-semibold text-lg mt-4 dark:text-white"
+                  >
+                    {purpose.id}. {purpose.heading}
+                  </h1>
+                  <p key={purpose.id} class="mt-2 text-md dark:text-gray-300">
+                    {purpose.paragraph}
+                  </p>
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
