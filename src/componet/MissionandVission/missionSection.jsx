19e79c98805f8e7547e3c744025cb8291f8464ca
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-27 18:02:56 +0100

    Update MissionSection component with revised purpose statement and content

--- src/componet/MissionandVission/missionSection.jsx
+++ src/componet/MissionandVission/missionSection.jsx
@@ -1,8 +1,8 @@
 export default function MissionSection() {
   return (
     <div class="w-full h-full bg-white dark:bg-gray-800">
-      <div class="w-full mx-auto py-10 bg-white dark:bg-gray-800">
-        <div class="w-[90%] mx-auto flex md:gap-4 xs:gap-2 justify-center items-center pt-4">
+      <div class="w-full mx-auto py-5 bg-white dark:bg-gray-800">
+        <div class="w-[90%] mx-auto flex md:gap-4 xs:gap-2 justify-center items-center pt-2">
           <div class="flex gap-2 items-center">
             <img
               src="/images/images.png"
@@ -19,49 +19,52 @@ export default function MissionSection() {
             5 MIN READ
           </h4>
         </div>
-        <h1 class="w-[92%] mx-auto lg:text-4xl md:text-3xl xs:text-2xl text-center font-serif font-semibold pb-4 pt-8 dark:text-white">
+        <h1 class="w-[92%] mx-auto lg:text-4xl md:text-3xl xs:text-2xl text-center font-serif font-semibold pb-2 pt-2 dark:text-white">
           Our Purpose Statement | Our Mission and Vision as a Church.
         </h1>
-        <div class="py-6 bg-white dark:bg-gray-800">
+        <div class="py-2 bg-white dark:bg-gray-800">
           <div class="md:w-[80%] xs:w-[90%] mx-auto pt-4">
-            <p class="mx-auto text-md dark:text-gray-300">
-              In the world of CSS frameworks, there are plenty of contenders
-              vying for your attention. But for me, Tailwind CSS stands out from
-              the pack. Here's why Tailwind CSS is my go-to for building modern
-              websites:
+            <h1 class="font-semibold text-lg mt-4 dark:text-white">
+              1. The Gateway Baptist Church (Community of Grace) exists to
+              fulfill the Great Commandment and the great Commission.
+            </h1>
+            <p class="mt-2 text-md dark:text-gray-300">
+              In Matthew 22:37-40, Jesus emphasizes the importance of loving God
+              with all our heart, soul, and mind, and loving our neighbors as
+              ourselves, stating that all the law and the Prophets are based on
+              these two commandments. In Matthew 28:19-20, Jesus commands his
+              followers to go and make disciples of all nations, baptizing them
+              in the name of the Father, Son, and Holy Spirit, and teaching them
+              to obey all that He has commanded. Both passages highlight the
+              core of Christian teaching: love for God and others, and the
+              mission to spread this love through discipleship and obedience.
             </p>
 
             <h1 class="font-semibold text-lg mt-4 dark:text-white">
-              1. Utility-First Philosophy
+              2. To organize and maintain the following programmes and
+              departments.
             </h1>
             <p class="mt-2 text-md dark:text-gray-300">
-              Tailwind ditches bulky pre-built components and instead offers a
-              massive toolbox of utility classes. These classes, like
-              "text-red-500" or "flex justify-center," target specific styles
-              (color, layout) and can be easily combined to achieve your desired
-              look. This gives you ultimate control and keeps your CSS nice and
-              lean.
+              The Nigerian Baptist Convention supports a variety of programs and
+              departments aimed at fostering spiritual growth and community
+              engagement. These include Sunday School, Church Training
+              Programmes, the Women Missionary Union, the Men Missionary Union,
+              House Fellowship or Cell Groups, as well as any other programs or
+              departments that may be approved by the Convention. These
+              initiatives work together to encourage learning, fellowship, and
+              mission work within the church community.
             </p>
 
             <h1 class="font-semibold text-lg mt-4 dark:text-white">
-              2. Rapid Prototyping
+              3. To encourage group activities for enhancing the growth of the
+              church.
             </h1>
             <p class="mt-2 text-md dark:text-gray-300">
-              Need to get a design off the ground quickly? Tailwind's utility
-              classes make it a breeze. Forget digging through stylesheets -
-              just apply classes directly in your HTML. This lets you iterate on
-              designs faster and see the visual changes instantly.
-            </p>
-
-            <h1 class="font-semibold text-lg mt-4 dark:text-white">
-              3. Responsive Out of the Box
-            </h1>
-            <p class="mt-2 text-md dark:text-gray-300">
-              Tailwind's utility classes are inherently responsive, meaning they
-              adapt to different screen sizes. No need for complex media queries
-              - just add a responsive variant to your class (e.g. "text-lg" for
-              large screens). This saves you time and ensures your website looks
-              sharp on any device.
+              The church seeks to encourage group activities that foster a sense
+              of community and collaboration, aimed at enhancing its growth both
+              spiritually and numerically. These group activities provide
+              opportunities for individuals to connect with one another, share
+              their faith, and support each other in their spiritual journeys.
             </p>
 
             <h1 class="font-semibold text-lg mt-4 dark:text-white">
