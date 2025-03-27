beef7192e9c6fe5e9989df421f84339a0b2a0c2d
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-27 17:40:24 +0100

    Add Mission and Vision component with video banner

---
+++ src/pages/missionAndVission.jsx
@@ -0,0 +1,13 @@
+import VideoBanner from "../componet/Banner/videoBanner";
+import Footer from "../componet/Footer/footer";
+import Header from "../componet/Header/header";
+
+export default function Missionandvission() {
+  return (
+    <>
+      <Header />
+      <VideoBanner video="mission-video" title="The Mission and Vision of the Church" subtitle="The Mission and Vision of the Church" />
+      <Footer />
+    </>
+  );
+}
