cb1d1996dd50e48dab9e5d4060441e383ff4120e
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-27 17:46:56 +0100

    Add MissionSection component to Mission and Vision page

--- src/pages/missionAndVission.jsx
+++ src/pages/missionAndVission.jsx
@@ -1,12 +1,18 @@
 import VideoBanner from "../componet/Banner/videoBanner";
 import Footer from "../componet/Footer/footer";
 import Header from "../componet/Header/header";
+import MissionSection from "../componet/MissionandVission/missionSection";
 
 export default function Missionandvission() {
   return (
     <>
       <Header />
-      <VideoBanner video="mission-video" title="The Mission and Vision of the Church" subtitle="The Mission and Vision of the Church" />
+      <VideoBanner
+        video="mission-video"
+        title="The Mission and Vision of the Church"
+        subtitle="The Mission and Vision of the Church"
+      />
+      <MissionSection />
       <Footer />
     </>
   );
