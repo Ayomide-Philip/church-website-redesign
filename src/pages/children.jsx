0c0a25bbd4f2f8e6ffd1a6e9cad6c8ccde1cc7ca
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-29 11:34:52 +0100

    Add Children component with Header and Footer imports

---
+++ src/pages/children.jsx
@@ -0,0 +1,11 @@
+import Footer from "../componet/Footer/footer";
+import Header from "../componet/Header/header";
+
+export default function Children() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
