8d5b57bd1372da4211665174984fe807eccf7b4f
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-29 19:20:26 +0100

    Add VideoBanner component to Children page

--- src/pages/children.jsx
+++ src/pages/children.jsx
@@ -1,11 +1,17 @@
 import ChildrenPage from "../componet/Children/childrenPage";
 import Footer from "../componet/Footer/footer";
 import Header from "../componet/Header/header";
+import VideoBanner from "../componet/Banner/videoBanner";
 
 export default function Children() {
   return (
     <>
       <Header />
+      <VideoBanner
+        video="mission-video"
+        title="The Mission and Vision of the Church"
+        subtitle="The Mission and Vision of the Church"
+      />
       <ChildrenPage />
       <Footer />
     </>
