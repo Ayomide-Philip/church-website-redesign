498208096453affc8071bd183b099c2be837d224
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-30 14:23:52 +0100

    Add heading for ChildrenLeader component to enhance section visibility

--- src/componet/Children/childrenLeader.jsx
+++ src/componet/Children/childrenLeader.jsx
@@ -1,6 +1,9 @@
 export default function ChildrenLeader() {
   return (
     <section className="text-white bg-gray-800 body-font">
+      <h2 class="text-3xl font-semibold tracking-tight text-center pt-10 text-white sm:text-5xl">
+        Our Leader
+      </h2>
       <div className="container mx-auto flex px-5 py-24 pt-10 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
