50de7af9bac689ceea5b040992df7b40fc209933
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-31 08:57:39 +0100

    Refactor Team component: update class attributes to className and enhance button for improved navigation

--- src/componet/Home/team.jsx
+++ src/componet/Home/team.jsx
@@ -31,44 +31,49 @@ const team = [
 
 export default function Team() {
   return (
-    <section class="text-gray-400 bg-gray-900 body-font">
-      <div class="container px-5 py-5 mx-auto">
-        <div class="flex flex-col text-center w-full mb-20">
-          <h1 class="text-3xl font-medium title-font mb-4 text-white tracking-widest">
-          GUIDING LIGHTS
+    <section className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-5 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="text-3xl font-medium title-font mb-4 text-white tracking-widest">
+            GUIDING LIGHTS
           </h1>
-          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Our church team is a dedicated group of pastors, leaders, and
             volunteers committed to serving God and our community. With faith,
             love, and unity, we work together to inspire, support, and guide
             others in their spiritual journey.
           </p>
         </div>
-        <div class="flex flex-wrap -m-4">
+        <div className="flex flex-wrap -m-4">
           {team.map((members) => {
             return (
-              <div class="p-4 lg:w-1/2">
-                <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+              <div className="p-4 lg:w-1/2">
+                <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                   <img
                     alt="team"
-                    class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
+                    className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
                     src={`/images/leaders/guidingLight/${members.image}.jpg`}
                   />
-                  <div class="flex-grow sm:pl-8">
-                    <h2 class="title-font font-medium text-lg text-white">
+                  <div className="flex-grow sm:pl-8">
+                    <h2 className="title-font font-medium text-lg text-white">
                       {members.name}
                     </h2>
-                    <h3 class="text-gray-500 mb-3">{members.role}</h3>
-                    <p class="mb-4">{members.description}</p>
+                    <h3 className="text-gray-500 mb-3">{members.role}</h3>
+                    <p className="mb-4">{members.description}</p>
                   </div>
                 </div>
               </div>
             );
           })}
         </div>
-        <button class="flex mx-auto mt-10 text-black bg-white border-0 py-2 px-10 focus:outline-none hover:bg-black hover:text-white rounded text-lg">
-          Meet The Team
-        </button>
+        <div className="flex justify-center mt-10">
+          <a
+            href="/leaders"
+            className="inline-flex bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600  text-lg  bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            Meet the Leaders
+          </a>
+        </div>
       </div>
     </section>
   );
