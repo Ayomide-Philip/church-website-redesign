fbd6944d0bcb16c0384c5dc415404fad485a6fe6
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-01 00:19:12 +0100

    Update TeenagerLeaders component: center content and add new leader profile

--- src/componet/Teenager/teenagerLeaders.jsx
+++ src/componet/Teenager/teenagerLeaders.jsx
@@ -7,13 +7,36 @@ export default function TeenagerLeaders() {
             OUR TEENAGERS LEADER
           </h1>
         </div>
-        <div class="flex flex-wrap -m-4">
+        <div class="flex flex-wrap justify-center text-center -m-4">
           <div class="p-4 lg:w-1/4 md:w-1/2">
             <div class="h-full flex flex-col items-center text-center">
               <img
                 alt="team"
                 class="flex-shrink-0 rounded-lg w-60 h-56 object-cover object-[center_top] mb-4"
                 src="/images/leaders/guidingLight/teen-pastor.jpg"
+              />
+
+              <div class="w-full">
+                <h2 class="title-font font-medium text-lg text-white ">
+                  Pastor Mrs T.A Ogunjimi
+                </h2>
+                <h3 class="text-white mb-3">Lorem ipsum</h3>
+                <p class="mb-4">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+                  nesciunt quisquam? Dolores cumque qui rerum possimus sequi ab
+                  enim voluptatibus ipsum esse quod. Blanditiis harum doloribus
+                  dignissimos hic ut omnis?
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-4 lg:w-1/4 md:w-1/2">
+            <div class="h-full flex flex-col items-center text-center">
+              <img
+                alt="team"
+                class="flex-shrink-0 rounded-lg w-60 h-56 object-cover object-[center_top] mb-4"
+                src="/public/images/leaders/guidingLight/teen-pastor.jpg"
               />
 
               <div class="w-full">
