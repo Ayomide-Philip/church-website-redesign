e21b04d7b6aa0147326ba43b787a5002c153a715
Areo Ayomide <areoayomide2008@gmail.com>
2025-03-31 07:15:00 +0100

    Add ChildrenPresentation component to ChildrenPage

--- src/componet/Children/childrenPage.jsx
+++ src/componet/Children/childrenPage.jsx
@@ -1,11 +1,13 @@
 import ChildrenFeature from "./childenFeature";
 import ChildrenLeader from "./childrenLeader";
+import ChildrenPresentation from "./childrenPresentation";
 
 export default function ChildrenPage() {
   return (
     <>
       <ChildrenFeature />
       <ChildrenLeader />
+      <ChildrenPresentation />
     </>
   );
 }
