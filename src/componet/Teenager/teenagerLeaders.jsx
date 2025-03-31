ef7bcae70a5914321235bfd729f0b4c8c4696c4d
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-01 00:17:33 +0100

    Update TeenagerLeaders component: enhance styling and improve text visibility

--- src/componet/Teenager/teenagerLeaders.jsx
+++ src/componet/Teenager/teenagerLeaders.jsx
@@ -1,16 +1,11 @@
 export default function TeenagerLeaders() {
   return (
-    <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-col text-center w-full mb-20">
-          <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">
-            OUR TEAM
+    <section class="text-white bg-gray-800 body-font">
+      <div class="container px-5 py-10 mx-auto">
+        <div class="flex flex-col text-center w-full mb-10">
+          <h1 class="text-2xl font-medium title-font mb-4 text-white tracking-widest sm:text-3xl">
+            OUR TEENAGERS LEADER
           </h1>
-          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-            gentrify, subway tile poke farm-to-table. Franzen you probably
-            haven't heard of them.
-          </p>
         </div>
         <div class="flex flex-wrap -m-4">
           <div class="p-4 lg:w-1/4 md:w-1/2">
@@ -22,13 +17,10 @@ export default function TeenagerLeaders() {
               />
 
               <div class="w-full">
-                <h2 class="title-font font-medium text-lg text-gray-900">
-                  lorem Ipsum
+                <h2 class="title-font font-medium text-lg text-white ">
+                  Pastor Mrs T.A Ogunjimi
                 </h2>
-                <h3 class="text-gray-500 mb-3">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Nostrum, natus.
-                </h3>
+                <h3 class="text-white mb-3">Lorem ipsum</h3>
                 <p class="mb-4">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
                   nesciunt quisquam? Dolores cumque qui rerum possimus sequi ab
