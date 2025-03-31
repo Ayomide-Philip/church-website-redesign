a1920a03bf8d0069384cdccb6a890225601c3e40
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-01 00:11:05 +0100

    Update Teenagers component: add TeenagerLeaders component to the page

--- src/pages/teenagers.jsx
+++ src/pages/teenagers.jsx
@@ -2,6 +2,7 @@ import Header from "../componet/Header/header";
 import Footer from "../componet/Footer/footer";
 import VideoBanner from "../componet/Banner/videoBanner";
 import TeenagerWelcome from "../componet/Teenager/teenagerWelcome";
+import TeenagerLeaders from "../componet/Teenager/teenagerLeaders";
 
 export default function Teenagers() {
   return (
@@ -13,6 +14,7 @@ export default function Teenagers() {
         subtitle="...Learn more about the Teenagers Ministry."
       />
       <TeenagerWelcome />
+      <TeenagerLeaders />
       <Footer />
     </>
   );
