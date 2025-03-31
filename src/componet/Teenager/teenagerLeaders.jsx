f089089629a47582ff9fded59ddc50afc55f206b
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-01 00:11:19 +0100

    Add TeenagerLeaders component to display team information

---
+++ src/componet/Teenager/teenagerLeaders.jsx
@@ -0,0 +1,45 @@
+export default function TeenagerLeaders() {
+  return (
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col text-center w-full mb-20">
+          <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">
+            OUR TEAM
+          </h1>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+            gentrify, subway tile poke farm-to-table. Franzen you probably
+            haven't heard of them.
+          </p>
+        </div>
+        <div class="flex flex-wrap -m-4">
+          <div class="p-4 lg:w-1/4 md:w-1/2">
+            <div class="h-full flex flex-col items-center text-center">
+              <img
+                alt="team"
+                class="flex-shrink-0 rounded-lg w-60 h-56 object-cover object-[center_top] mb-4"
+                src="/images/leaders/guidingLight/teen-pastor.jpg"
+              />
+
+              <div class="w-full">
+                <h2 class="title-font font-medium text-lg text-gray-900">
+                  lorem Ipsum
+                </h2>
+                <h3 class="text-gray-500 mb-3">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Nostrum, natus.
+                </h3>
+                <p class="mb-4">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+                  nesciunt quisquam? Dolores cumque qui rerum possimus sequi ab
+                  enim voluptatibus ipsum esse quod. Blanditiis harum doloribus
+                  dignissimos hic ut omnis?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
