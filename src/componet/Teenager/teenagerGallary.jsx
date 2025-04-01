d982c168f0083c37d62a05b26b51598f239c2af6
Areo Ayomide <areoayomide2008@gmail.com>
2025-04-01 20:04:47 +0100

    Add TeenagerGallary component: implement image gallery layout for teenagers

---
+++ src/componet/Teenager/teenagerGallary.jsx
@@ -0,0 +1,36 @@
+export default function TeenagerGallary() {
+  return (
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-3 p-4 max-w-[400px] md:max-w-[600px] place-items-center">
+      <img
+        class="hover:opacity-75"
+        src="https://source.unsplash.com/collection/1346951/150x150?sig=1"
+        alt=""
+      />
+      <img
+        class="hover:opacity-75"
+        src="https://source.unsplash.com/collection/1346951/150x150?sig=2"
+        alt=""
+      />
+      <img
+        class="hover:opacity-75"
+        src="https://source.unsplash.com/collection/1346951/150x150?sig=3"
+        alt=""
+      />
+      <img
+        class="hover:opacity-75"
+        src="https://source.unsplash.com/collection/1346951/150x150?sig=4"
+        alt=""
+      />
+      <img
+        class="hover:opacity-75"
+        src="https://source.unsplash.com/collection/1346951/150x150?sig=5"
+        alt=""
+      />
+      <img
+        class="hover:opacity-75"
+        src="https://source.unsplash.com/collection/1346951/150x150?sig=6"
+        alt=""
+      />
+    </div>
+  );
+}
